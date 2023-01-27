552b2e5fdd1359779a95f52fc4acbe17f2207cc2
anasalansari <77924312+anas-ansri@users.noreply.github.com>
2023-01-28 00:11:37 +0530

    update

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Ansari\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Software\flutter-examples"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=20.4.38"
+export "FLUTTER_BUILD_NUMBER=20.4.38"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
